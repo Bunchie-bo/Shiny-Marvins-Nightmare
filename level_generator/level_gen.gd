11a3888b09fbf539d733a3984d3d8371b5f11dcf
Bunchie-bo <marshall.moodie@gmail.com>
2024-03-03 09:03:53 -0600

    Add files via upload

--- level_generator/level_gen.gd
+++ level_generator/level_gen.gd
@@ -30,30 +30,24 @@ func load_tile_data():
 
 func map_initialize(new_map_size : Vector2, tiles : Dictionary):
 	size = new_map_size
-	'''
-	for _y in range(size.y):
+	var x = []
+	for _y in range(size.x):
 		var y = []
-		for _x in range(size.x):
-			var x = []
-			x.append(tiles.duplicate())
-			y.append(x)
-			wave_function.append(y)
-	'''
-	#need to make a y array and fill it with x.map_size number of x arrays holding the tile data,
-	#the function above is making 4 x ararys per y array
-	for _y in size.y:
-		var y = []
-		for _x in size.x:
-			var x = []
-			x.append(tiles.duplicate())
-			y.append(x)
-			wave_function.append(y)
+		for _x in range(size.y):
+			y.append(tiles.duplicate())
+		x.append(y)
+	wave_function.append(x)
+
+
+
+
+
 
 
 
 func test():
 	for i in wave_function:
-		print(i[0])
+		print(i[0][0])
 
 
 

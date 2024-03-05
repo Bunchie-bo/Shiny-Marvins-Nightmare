fee21978b9449c28043e9599fa32b2c61bf06941
Bunchie-bo <marshall.moodie@gmail.com>
2024-03-05 13:23:19 -0600

    Add files via upload

--- level_generator/level_gen.gd
+++ level_generator/level_gen.gd
@@ -4,7 +4,7 @@ extends Node2D
 @onready var tile_map = $TileMap
 @onready var tile_values = "res://tileSets/tileValues.json"
 
-var map_size = Vector2(4,4)
+var map_size = Vector2(6,6)
 var wave_function : Array
 
 var size : Vector2
@@ -30,9 +30,9 @@ func load_tile_data():
 
 func map_initialize(new_map_size : Vector2, tiles : Dictionary):
 	size = new_map_size
-	for _y in range(size.x):
+	for _x in range(size.x):
 		var y = []
-		for _x in range(size.y):
+		for _y in range(size.y):
 			y.append(tiles.duplicate())
 		wave_function.append(y)
 
@@ -49,12 +49,26 @@ func propagate():
 
 
 func get_least_tile_count():
+	
 	pass
+
+
 
 
 func test():
 	#for i in wave_function:
-	print(wave_function[0][0])
+	#print(wave_function[0][0].values())
+
+	var list = wave_function[0][0].keys()
+	#print(list)
+	#list.erase("fill")
+	var epic = wave_function[0][0]["fill"]
+	print(epic["weight"])
+
+
+
+
+
 
 
 func get_cords(x : int, y : int): #not sure if ill keep this, its kinda bloat, maybe a get tile arg?

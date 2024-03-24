4de0bd45082945b509da4c4b3e362257c35cd04d
Bunchie-bo <marshall.moodie@gmail.com>
2024-03-24 16:26:37 -0500

    Collapse tile func is finished and ready
    collapse is ready and adds surrounding tiles to the stack for the propagation function which just needs to be finished, the for loops for checking the tile if there is more than one tile in the cell, just needs to be finished checking over all the indivitual tiles and chekcing if the tiles are compatible with the surrounding 4 tiles if they match, just use a for loop to check every negx value of every tile with the posx and the negy with the negx, etc. might be a bit sloppy in terms of performance but levels are gonna be kept under 1000x1000 anyways so it shouldent be too bad. Also tile weights are now working and should be adjusted accoudingly in the tilevalues.json file

--- level_generator/level_gen.gd
+++ level_generator/level_gen.gd
@@ -4,22 +4,28 @@ extends Node2D
 @onready var tile_map = $TileMap
 @onready var tile_values = "res://tileSets/tileValues.json"
 
-var map_size = Vector2(6,6)
+var map_size = Vector2(3,3)
 var wave_function : Array
 
 var size : Vector2
 var stack : Array
 
-var collapsed_tile_count : int
+var collapsed_tile_count = 0
 
 
 func _ready():
 #	tile_map.set_cell(0,Vector2(0,0), 0,Vector2(0,0))
 #	load_tile_data()
 	map_initialize(map_size, load_tile_data())
-	collapse()
-	
 	test()
+
+func iterate():
+	var coords = get_min_entropy_tiles()
+	collapse(coords)
+	propagate(coords)
+
+
+
 
 
 func load_tile_data():
@@ -39,45 +45,83 @@ func map_initialize(new_map_size : Vector2, tiles : Dictionary):
 
 
 
-func collapse():
+func collapse(coords: Vector2):
 	#Collapses a tile in a list of cells with the least ammount of remaining tiles, in the
 	#case of the first run, this would be a random tile out of any of the tiles on the board
-	pass
-
-
-func propagate():
-	pass
-
-
-func get_least_tile_count():
 	
+	var tile = wave_function[coords.x][coords.y]
+	var tile_list = {}
+	for i in tile.keys():
+		tile_list[i] = tile[i]["weight"]
+	var chosen_tile = tile[WeightedChoice.pick(tile_list)]
+	wave_function[coords.x][coords.y].clear()
+	wave_function[coords.x][coords.y] = chosen_tile
+	collapsed_tile_count += 1
+	#adds the 4 neighboring tiles to the stack to be propagated
+	stack.append(Vector2(coords.x + 1, coords.y))
+	stack.append(Vector2(coords.x - 1, coords.y))
+	stack.append(Vector2(coords.x, coords.y + 1))
+	stack.append(Vector2(coords.x, coords.y - 1))
+	#print(wave_function[coords.x][coords.y])
+
+func propagate(coords: Vector2):
+	print(stack)
+	for i in stack:
+		if wave_function[i.x][i.y].size() > 1:
+			#print(wave_function[i.x][i.y])
+			for tile in wave_function[i.x][i.y]:
+				#check negx negy posx posy too see if the neighboring
+				#tiles ``wave_function[i.x +1][i.y]`` and other sides too
+				#if the string files are the same, and if so do nothing
+				#and if not remove from wave_function[i.x][i.y]
+				pass
+			
+			stack.erase(i)
+		else:
+			stack.erase(i)
+
+
+func check_tile_neighbor():
 	pass
-
-
-
 
 func test():
+
+
+	collapse_random_tile()
+	propagate(Vector2(0,0))
+
+
+
+
 	#for i in wave_function:
 	#print(wave_function[0][0].values())
 
-	var list = wave_function[0][0].keys()
+	#var list = wave_function[0][0].keys()
 	#print(list)
 	#list.erase("fill")
-	var epic = wave_function[0][0]["fill"]
+	#var epic = wave_function[0][0]["fill"]
 	#print(epic["weight"])
-	collapse_random_tile()
+	#collapse_random_tile()
+	#var coords = Vector2(1,1)
+	#collapse(coords)
+
+
 
 
 
 func collapse_random_tile():
-	var rando = wave_function[randi_range(0,map_size.x - 1)][randi_range(0,map_size.y - 1)]
-	var keys = rando.keys()
+	var coords = Vector2(randi_range(0,map_size.x - 1), randi_range(0,map_size.y - 1))
+	var rando = wave_function[coords.x][coords.y]
 	var tile_list = {}
-	for i in keys:
-		#print(str(i) + str(rando[i]))
+	for i in rando.keys():
 		tile_list[i] = rando[i]["weight"]
-	#print(tile_list)
+	var chosen_tile = WeightedChoice.pick(tile_list)
+	print(chosen_tile)
 	
+	stack.append(Vector2(coords.x + 1, coords.y))
+	stack.append(Vector2(coords.x - 1, coords.y))
+	stack.append(Vector2(coords.x, coords.y + 1))
+	stack.append(Vector2(coords.x, coords.y - 1))
 
 
 
@@ -88,6 +132,26 @@ func get_cords(x : int, y : int): #not sure if ill keep this, its kinda bloat, m
 
 
 
+
+func get_entropy(coords : Vector2):
+	return len(wave_function[coords.x][coords.y])
+
+func get_min_entropy_tiles():
+	var min_entropy
+	var coords
+	for x in range(size.x):
+		for y in range(size.y):
+			var entropy = get_entropy(Vector2(x,y))
+			if entropy > 1:
+				entropy += randf_range(-0.1, 0.1)
+
+				if not min_entropy:
+					min_entropy = entropy
+					coords = Vector2(x, y)
+				elif entropy < min_entropy:
+					min_entropy = entropy
+					coords = Vector2(x, y)
+	return coords
 
 
 
